85de25f4b08f64f9d18133ad23905bc33760d05d
saugat1070 <saugatgiri1070@gmail.com>
2025-02-08 01:10:35 +0545

    register Issue Book model on admin panel

--- Library_management/api_LMS/admin.py
+++ Library_management/api_LMS/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from api_LMS.models import UserRegistration
 from api_LMS.models import Book_details
+from api_LMS.models import IssueBook
 # Register your models here.
 admin.site.register(UserRegistration)
 admin.site.register(Book_details)
+admin.site.register(IssueBook)
