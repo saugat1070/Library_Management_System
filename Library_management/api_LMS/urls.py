aa5c212b6e81db11c855db8b42f27595e322bf86
saugat1070 <saugatgiri1070@gmail.com>
2025-02-08 00:54:03 +0545

    adding issue book urls in api app

--- Library_management/api_LMS/urls.py
+++ Library_management/api_LMS/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('register_page/',views.register.as_view(),name='register_page'),
     path('list_of_user/',views.list_of_user.as_view(),name='list_of_user'),
     path('user_profile/',views.UserProfile.as_view(),name='user_profile'),
-    path('book_add/',views.Book_Details.as_view(),name='book_added')
+    path('book_add/',views.Book_Details.as_view(),name='book_added'),
+    path('issue_book/',views.IssueBook.as_view(),name='issue_book'),
 
 ] 
