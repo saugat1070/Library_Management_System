318f8e6181b4ea035636cc4e232eb20e2a737e15
saugat1070 <saugatgiri1070@gmail.com>
2025-02-08 02:00:31 +0545

    still remains author of book problem

--- Library_management/api_LMS/views.py
+++ Library_management/api_LMS/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.response import Response
 from rest_framework import generics
 from api_LMS.models import UserRegistration
 from api_LMS.models import Book_details
@@ -9,6 +10,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from api_LMS.models import IssueBook
 from api_LMS.serializers import IssueBookSerializer
+from rest_framework import status
 # Create your views here.
 
 def home(self):
@@ -38,10 +40,37 @@ class Book_Details(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         return serializer.save(added_by = self.request.user)
     
-class IssueBook(APIView):
-    permission_classes = [IsAuthenticated]
-    serializer = IssueBookSerializer
+# class IssueBookView(APIView):
+#     permission_classes = [IsAuthenticated]
 
-    def get(self,request,format=None):
-        
+#     def get(self,request,format=None):
+#         issuebook = IssueBook.objects.all()
+#         serializer = IssueBookSerializer(issuebook, many = True)
+#         return Response(serializer.data)
+
+#     def post(self,request,format=None):
+#         serializer = IssueBookSerializer(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save(issue_by=request.user,
+#                             author_of_book=request.data.get('name_of_author'),
+#                             college_name=request.user.college_name)
+#             return Response(serializer.data,status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+class IssueBookView(generics.ListCreateAPIView):
+    queryset = IssueBook.objects.all()
+    permission_classes = [IsAuthenticated]
+    serializer_class = IssueBookSerializer
+
+    def perform_create(self, serializer):
+        name_of_book = self.request.data.get('name_of_book')
+        author_of_book = None
+        try:
+            book = Book_Details.objects.get(book_name=name_of_book)
+            author_name = book.author_name
+        except:
+            author_name = 'Unknown'
+        return serializer.save(issue_by=self.request.user,
+                               author_of_book=author_name,
+                               college_name=self.request.user.college_name)
+
     
