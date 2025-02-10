6338d54e4b68cc70d095c7b4fc1f13116ceb28e0
saugat1070 <saugatgiri1070@gmail.com>
2025-02-10 11:46:12 +0545

    update urls.py file

--- Library_management/api_LMS/urls.py
+++ Library_management/api_LMS/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('register_page/',views.register.as_view(),name='register_page'),
     path('list_of_user/',views.list_of_user.as_view(),name='list_of_user'),
     path('user_profile/',views.UserProfile.as_view(),name='user_profile'),
-    path('book_add/',views.Book_Details.as_view(),name='book_added'),
+    path('book_info/',views.Book_Details.as_view(),name='book_added'),
+    path('book_info/<int:pk>',views.Update_Book.as_view(),name='update_book'),
     path('issue_book/',views.IssueBookView.as_view(),name='issue_book'),
 
 ] 
