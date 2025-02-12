25a016d30789289e05cd5cd34a9dfc7814d44701
saugat1070 <saugatgiri1070@gmail.com>
2025-02-12 10:54:27 +0545

    register book submission in admin panel

--- Library_management/api_LMS/admin.py
+++ Library_management/api_LMS/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from api_LMS.models import UserRegistration
 from api_LMS.models import Book_details
 from api_LMS.models import IssueBook
+from api_LMS.models import Book_Submission
 # Register your models here.
 admin.site.register(UserRegistration)
 admin.site.register(Book_details)
 admin.site.register(IssueBook)
+admin.site.register(Book_Submission)
