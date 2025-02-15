31ff6379cd1c794427b917a2cb76ec656625ca35
saugat1070 <saugatgiri1070@gmail.com>
2025-02-16 05:31:12 +0545

    modified admin panel using admin decorator

--- Library_management/api_LMS/admin.py
+++ Library_management/api_LMS/admin.py
@@ -4,7 +4,13 @@ from api_LMS.models import Book_details
 from api_LMS.models import IssueBook
 from api_LMS.models import Book_Submission
 # Register your models here.
-admin.site.register(UserRegistration)
+#admin.site.register(UserRegistration)
+@admin.register(UserRegistration)
+class UserRegistrationAdmin(admin.ModelAdmin):
+    list_display = ['username','password']
 admin.site.register(Book_details)
 admin.site.register(IssueBook)
-admin.site.register(Book_Submission)
+# admin.site.register(Book_Submission)
+@admin.register(Book_Submission)
+class Book_SubmissionAdmin(admin.ModelAdmin):
+    list_display = ['issued_book','date_of_issue','date_of_submission','late_fee','submiited']
