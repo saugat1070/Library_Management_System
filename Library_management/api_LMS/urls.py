f6e0124e00d475fd7d7db0b1b34122550b38f51f
saugat1070 <saugatgiri1070@gmail.com>
2025-04-04 09:05:38 +0545

    configure urls of lms for login

--- Library_management/api_LMS/urls.py
+++ Library_management/api_LMS/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from api_LMS import views
-
+# from rest_framework.authtoken.views import obtain_auth_token
+from api_LMS.views import CustomJWTAuthToken
 urlpatterns = [
     path('',views.home),
     path('register_page/',views.register.as_view(),name='register_page'),
@@ -9,5 +10,6 @@ urlpatterns = [
     path('book_info/',views.Book_Details.as_view(),name='book_added'),
     path('book_info/<int:pk>',views.Update_Book.as_view(),name='update_book'),
     path('issue_book/',views.IssueBookView.as_view(),name='issue_book'),
+    path('login/',CustomJWTAuthToken.as_view(),name='login'),
 
 ] 
